09735a008680e8a683a3c65c34d760afc9c5c678
Alessio Cimarelli <alessio.cimarelli@sparkfabrik.com>
2023-08-31 10:23:50 +0200

    Add openapi spec (draft) and utilities

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 ACTION=$1
 SOURCE_NAME=$2
+SWAGGER_PORT=${2:-8080}
 
 DOCKER_IMAGE_NAME=ondata-conf-amm-istat
 DOCKER_IMAGE_VERSION=latest
@@ -20,6 +21,16 @@ generate() {
     fi
 }
 
+serve () {
+    echo "Swagger UI running at http://localhost:$SWAGGER_PORT"
+    docker run --rm -p $SWAGGER_PORT:8080 -v $PWD/api/openapi.v1.yaml:/tmp/openapi.v1.yaml -e SWAGGER_JSON=/tmp/openapi.v1.yaml swaggerapi/swagger-ui:v5.4.2
+}
+
+develop () {
+    echo "Swagger Editor running at http://localhost:$SWAGGER_PORT"
+    docker run --rm -p $SWAGGER_PORT:8080 -v $PWD/api/openapi.v1.yaml:/tmp/openapi.v1.yaml -e SWAGGER_FILE=/tmp/openapi.v1.yaml swaggerapi/swagger-editor:v4-latest
+}
+
 case $ACTION in
     build)
         build
@@ -27,7 +38,19 @@ case $ACTION in
     generate)
         generate
         ;;
+    serve)
+        serve
+        ;;
+    develop)
+        develop
+        ;;
     *)
-        echo "Usage: $0 [build | generate] [YYYYMMDD]"
+        echo "Usage: $0 [build | generate | develop] [YYYYMMDD | PORT]"
+        echo "Examples:"
+        echo "-  $0 build             # Build the Docker image"
+        echo "-  $0 generate          # Generate all resources listed in sources.json file"
+        echo "-  $0 generate 20230101 # Generate 20230101 resources only"
+        echo "-  $0 develop           # Run Swagger Editor on port 8080 (default)"
+        echo "-  $0 develop 8081      # Run Swagger Editor on port 8081 (custom)"
         ;;
 esac
