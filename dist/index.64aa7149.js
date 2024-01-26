4da2a1c8bfa3272caf130e8a0519bac46f0d3957
Alessio Cimarelli <alessio.cimarelli@sparkfabrik.com>
2024-01-26 17:55:23 +0100

    Clean dist folder

--- dist/index.64aa7149.js
+++
@@ -1,2 +0,0 @@
-window.onload=function(){window.ui=SwaggerUIBundle({url:"api/v1/openapi.v1.yml",dom_id:"#docs",deepLinking:!0,presets:[SwaggerUIBundle.presets.apis,SwaggerUIStandalonePreset],plugins:[SwaggerUIBundle.plugins.DownloadUrl],layout:"StandaloneLayout"})};
-//# sourceMappingURL=index.64aa7149.js.map
