8a6c5389dca4f37f3cf8f26270172e6682e5e9c3
Alessio Cimarelli <alessio.cimarelli@sparkfabrik.com>
2024-01-26 17:55:16 +0100

    Minor fixes

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -87,15 +87,15 @@ export function App() {
                 {resources.length > 0 && downloadPath !== '#'
                     ? (
                         <a
-                            className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
+                            className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center justify-between"
                             target="_blank"
                             href={`${window.API_DOMAIN}${downloadPath}`}
                             download={`ondata_confini_amministrativi_${downloadPath.replace(/^\//, '').replace(/\//g, '_')}`}
                         >
+                            <span>Download</span>
                             <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                 <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
                             </svg>
-                            <span>Download</span>
                         </a>
                     )
                     : (
