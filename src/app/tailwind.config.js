95aad456bbba6bac202e44ea5467a99683befa6e
Alessio Cimarelli <alessio.cimarelli@sparkfabrik.com>
2024-01-30 12:55:09 +0100

    Fix colors

--- src/app/tailwind.config.js
+++ src/app/tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
     "./**/*.{html,js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'oblue': {
+          light: '#C0DAF1',
+          DEFAULT: '#2E85D1',
+          dark: '#0E283F',
+        },
+        'ored': {
+          light: '#F9CDC4',
+          DEFAULT: '#EB593C',
+          dark: '#501408',
+        },
+      },
+    },
   },
   plugins: [],
 };
