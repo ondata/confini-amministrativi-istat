0efefb99dc9771af6a875c5fa3f660007ebfca8f
Alan Sprecacenere <alan@qreate.it>
2024-05-09 13:32:45 +0200

    rimozione delle funzioni anonime
    Non serve tenerle anonime se non si definiscono variabili locali

--- src/app/index.js
+++ src/app/index.js
@@ -1,48 +1,38 @@
-import { createRoot } from "react-dom/client";
-import { App } from "./App";
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
 
 // Constants
-window.API_DOMAIN = process.env.NODE_ENV !== 'production' ? 'http://localhost:8080' : '';
+window.API_DOMAIN =
+    process.env.NODE_ENV !== 'production' ? 'http://localhost:8080' : '';
 
-// Current year
-(function () {
-    document.getElementById('year').innerText = (new Date()).getFullYear();
-})();
+// Fill current year
+document.getElementById('year').innerText = new Date().getFullYear();
 
-// Swagger UI
-(function () {
-    window.ui = SwaggerUIBundle({
-        url: `${window.API_DOMAIN}/api/v2/openapi.v2.yml`,
-        dom_id: '#docs',
-        deepLinking: true,
-        presets: [
-            SwaggerUIBundle.presets.apis,
-            SwaggerUIStandalonePreset
-        ],
-        plugins: [
-            SwaggerUIBundle.plugins.DownloadUrl
-        ],
-        layout: "BaseLayout"
-    });
-})();
+// Init Swagger UI
+window.ui = SwaggerUIBundle({
+    url: `${window.API_DOMAIN}/api/v2/openapi.v2.yml`,
+    dom_id: '#docs',
+    deepLinking: true,
+    presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+    plugins: [SwaggerUIBundle.plugins.DownloadUrl],
+    layout: 'BaseLayout',
+});
 
-// Leaflet
-(function () {
-    // Register watermark control
-    L.Control.Watermark = L.Control.extend({
-        onAdd: () => {
-            var img = L.DomUtil.create('img');
-            img.src = 'https://www.ondata.it/wp-content/uploads/2022/03/logo-onData-corretto-trasparente.png';
-            img.style.width = '125px';
-            return img;
-        }
-    });
+// Register leaflet's watermark control
+L.Control.Watermark = L.Control.extend({
+    onAdd: () => {
+        var img = L.DomUtil.create('img');
+        img.src =
+            'https://www.ondata.it/wp-content/uploads/2022/03/logo-onData-corretto-trasparente.png';
+        img.style.width = '125px';
+        return img;
+    },
+});
 
-    L.control.watermark = function (opts) {
-        return new L.Control.Watermark(opts);
-    }
-})();
+L.control.watermark = function (opts) {
+    return new L.Control.Watermark(opts);
+};
 
-// React
+// Init app
 const appRoot = createRoot(document.getElementById('app'));
 appRoot.render(<App />);
