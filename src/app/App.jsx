4dde4b8b3ed937f32e8955973f324346c2f59586
Alan Sprecacenere <alan@qreate.it>
2024-05-09 13:17:49 +0200

    formattazione e linting

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useRef, useState } from "react";
-import Select from "react-select";
+import { useEffect, useRef, useState } from 'react';
+import Select from 'react-select';
 
 export function App() {
     const initialCenter = [42.5206995, 13.2275392];
@@ -11,7 +11,9 @@ export function App() {
     function ControlsItem({ id, label, children }) {
         return (
             <li className="flex flex-col gap-2">
-                <label className="capitalize" htmlFor={id}>{label}</label>
+                <label className="capitalize" htmlFor={id}>
+                    {label}
+                </label>
                 {children}
             </li>
         );
@@ -23,7 +25,13 @@ export function App() {
 
         const firstResource = `${window.API_DOMAIN}/api/v2/index.json`;
         const geoResourceType = 'application/geo+json';
-        const labels = ['country', 'release', 'division', 'territory', 'subdivision'];
+        const labels = [
+            'country',
+            'release',
+            'division',
+            'territory',
+            'subdivision',
+        ];
         const profiles = labels.map((s) => `/api/v2/hal-${s}.schema.json`);
 
         const [options, setOptions] = useState({});
@@ -37,7 +45,9 @@ export function App() {
             const data = await res.json();
             const items = data._links.item ?? [];
             const enclosures = data._links.enclosure ?? [];
-            const geoResource = enclosures.length && enclosures.find((e) => e.type === geoResourceType)?.href;
+            const geoResource =
+                enclosures.length &&
+                enclosures.find((e) => e.type === geoResourceType)?.href;
 
             if (items.length) {
                 const target = items[0].profile;
@@ -47,19 +57,28 @@ export function App() {
                     ...oldOptions,
                     [target]: items.map((item) => ({
                         value: `${window.API_DOMAIN}${item.href}`,
-                        label: item.title
+                        label: item.title,
                     })),
-                    ...profiles.slice(targetIndex + 1).reduce((options, target) => ({
-                        ...options,
-                        [target]: []
-                    }), {})
+                    ...profiles.slice(targetIndex + 1).reduce(
+                        (options, target) => ({
+                            ...options,
+                            [target]: [],
+                        }),
+                        {}
+                    ),
                 }));
 
-                profiles.slice(targetIndex).forEach((p) => selectRefs.current[p]?.clearValue?.());
+                profiles
+                    .slice(targetIndex)
+                    .forEach((p) => selectRefs.current[p]?.clearValue?.());
             }
 
             updateMap(geoResource ? `${window.API_DOMAIN}${geoResource}` : '');
-            setResources(enclosures.length ? enclosures.map((e) => ({ value: e.href, label: e.title })) : []);
+            setResources(
+                enclosures.length
+                    ? enclosures.map((e) => ({ value: e.href, label: e.title }))
+                    : []
+            );
             downloadRef.current?.clearValue?.();
             setDownloadPath('#');
         }
@@ -74,21 +93,28 @@ export function App() {
                 mapLayer && map.removeLayer(mapLayer);
 
                 // Add new layer
-                mapLayer = L.geoJson(
-                    geojson,
-                    {
-                        style: () => ({
-                            color: "#eb593c",
-                            weight: 2,
-                            fillColor: "#2e85d1",
-                            fillOpacity: 0.66
-                        })
-                    }
-                ).bindPopup((layer) => `
+                mapLayer = L.geoJson(geojson, {
+                    style: () => ({
+                        color: '#eb593c',
+                        weight: 2,
+                        fillColor: '#2e85d1',
+                        fillOpacity: 0.66,
+                    }),
+                })
+                    .bindPopup(
+                        (layer) => `
                     <table>
-                        ${Object.entries(layer.feature.properties).sort(([a], [b]) => a.localeCompare(b)).map(([k, v]) => `<tr><th>${k}</th><td>${v}</td></tr>`).join('\n')}
+                        ${Object.entries(layer.feature.properties)
+                            .sort(([a], [b]) => a.localeCompare(b))
+                            .map(
+                                ([k, v]) =>
+                                    `<tr><th>${k}</th><td>${v}</td></tr>`
+                            )
+                            .join('\n')}
                     </table>
-                `).addTo(map);
+                `
+                    )
+                    .addTo(map);
 
                 // Adapt map zoom and pan
                 map.fitBounds(mapLayer.getBounds());
@@ -96,8 +122,7 @@ export function App() {
                 mapLayer && map.removeLayer(mapLayer);
                 map.setView(initialCenter, initialZoom);
             }
-        };
-
+        }
 
         useEffect(() => {
             loadOptions(firstResource);
@@ -105,8 +130,7 @@ export function App() {
 
         return (
             <ol className="flex flex-col gap-6 mb-12">
-                {
-                profiles.map((p, i) => (
+                {profiles.map((p, i) => (
                     <ControlsItem id={p} label={labels[i]}>
                         <Select
                             id={p}
@@ -118,47 +142,50 @@ export function App() {
                             onChange={(e) => loadOptions(e?.value)}
                         />
                     </ControlsItem>
-                ))
-            }
+                ))}
 
-            <ControlsItem key="format" id="format" label="Format">
-                <Select
-                    id="format"
-                    ref={downloadRef}
-                    name="format"
-                    placeholder="Select a file format..."
-                    isDisabled={!resources.length}
-                    options={resources}
-                    onChange={(e) => setDownloadPath(e?.value ?? '#')}
-                />
-            </ControlsItem>
+                <ControlsItem key="format" id="format" label="Format">
+                    <Select
+                        id="format"
+                        ref={downloadRef}
+                        name="format"
+                        placeholder="Select a file format..."
+                        isDisabled={!resources.length}
+                        options={resources}
+                        onChange={(e) => setDownloadPath(e?.value ?? '#')}
+                    />
+                </ControlsItem>
 
-            <li key="download" className="flex flex-col gap-2">
-                {resources.length > 0 && downloadPath !== '#'
-                    ? (
+                <li key="download" className="flex flex-col gap-2">
+                    {resources.length && downloadPath !== '#' ? (
                         <a
                             className="bg-ored text-oblue-dark font-bold py-2 px-4 rounded inline-flex items-center justify-between"
                             target="_blank"
                             href={`${window.API_DOMAIN}${downloadPath}`}
-                            download={`ondata_confini_amministrativi_${downloadPath.replace(/^\//, '').replace(/\//g, '_')}`}
-                        >
+                            download={`ondata_confini_amministrativi_${downloadPath
+                                .replace(/^\//, '')
+                                .replace(/\//g, '_')}`}>
                             <span>Download</span>
-                            <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                            <svg
+                                className="fill-current w-4 h-4 mr-2"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 20 20">
                                 <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
                             </svg>
                         </a>
-                    )
-                    : (
+                    ) : (
                         <button className="bg-oblue-dark text-oblue-light border-oblue-dark font-bold py-2 px-4 rounded inline-flex items-center justify-between opacity-50 cursor-not-allowed">
                             <span>Download</span>
-                            <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                            <svg
+                                className="fill-current w-4 h-4 mr-2"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 20 20">
                                 <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
                             </svg>
                         </button>
-                    )
-                }
-            </li>
-        </ol>
+                    )}
+                </li>
+            </ol>
         );
     }
 
@@ -173,55 +200,112 @@ export function App() {
             );
 
             // Setup tiles
-            L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 maxZoom: 19,
                 attribution:
                     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
             }).addTo(map);
 
             // Add watermark
-            L.control.watermark({ position: "bottomleft" }).addTo(map);
+            L.control.watermark({ position: 'bottomleft' }).addTo(map);
 
-            return () => {
-                map.remove();
-            }
+            return () => map.remove();
         }, []);
 
-        return <div ref={mapRef} className="grow overflow-hidden max-sm:hidden"></div>;
+        return (
+            <div
+                ref={mapRef}
+                className="grow overflow-hidden max-sm:hidden"></div>
+        );
     }
 
     return (
         <main className="min-h-screen bg-oblue-light text-oblue-dark flex flex-row justify-center">
             <section className="p-6 max-w-sm">
                 <p className="mb-4">
-                    Questo progetto nasce nel <a rel="noopener noreferrer" target="_blank"
-                        href="https://github.com/teamdigitale/confini-amministrativi-istat" className="underline">novembre del
-                        2019</a>{' '}
-                    nell'ambito delle attività del <a rel="noopener noreferrer" target="_blank"
-                        href="https://teamdigitale.governo.it/" className="underline">Team per la Trasformazione Digitale</a>.
-                    Dopo più di un anno di inattività viene preso in mano dall'<a rel="noopener noreferrer" target="_blank"
-                        href="https://www.ondata.it/" className="underline">associazione OnData</a>{' '}
-                    nel <a rel="noopener noreferrer" target="_blank"
-                        href="https://github.com/ondata/confini-amministrativi-istat" className="underline">marzo del 2021</a>.
-                    Questa è l'ultima evoluzione del progetto originale sviluppata nel corso del 2023.
+                    Questo progetto nasce nel{' '}
+                    <a
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        href="https://github.com/teamdigitale/confini-amministrativi-istat"
+                        className="underline">
+                        novembre del 2019
+                    </a>{' '}
+                    nell'ambito delle attività del{' '}
+                    <a
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        href="https://teamdigitale.governo.it/"
+                        className="underline">
+                        Team per la Trasformazione Digitale
+                    </a>
+                    . Dopo più di un anno di inattività viene preso in mano
+                    dall'
+                    <a
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        href="https://www.ondata.it/"
+                        className="underline">
+                        associazione OnData
+                    </a>{' '}
+                    nel{' '}
+                    <a
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        href="https://github.com/ondata/confini-amministrativi-istat"
+                        className="underline">
+                        marzo del 2021
+                    </a>
+                    . Questa è l'ultima evoluzione del progetto originale
+                    sviluppata nel corso del 2023.
                 </p>
                 <p className="mb-4">
-                    La fonte ufficiale delle risorse geografiche è l'<a rel="noopener noreferrer" target="_blank"
-                        href="https://www.istat.it/it/archivio/222527" className="underline">ISTAT</a>.
-                    Altre fonti ufficiali con cui i dati sono arricchiti sono l'<a rel="noopener noreferrer" target="_blank"
-                        href="https://www.anagrafenazionale.interno.it/" className="underline">ANPR</a> e
-                    il progetto <a rel="noopener noreferrer" target="_blank"
-                        href="https://www.agid.gov.it/it/dati/vocabolari-controllati" className="underline">OntoPiA</a>.
+                    La fonte ufficiale delle risorse geografiche è l'
+                    <a
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        href="https://www.istat.it/it/archivio/222527"
+                        className="underline">
+                        ISTAT
+                    </a>
+                    . Altre fonti ufficiali con cui i dati sono arricchiti sono
+                    l'
+                    <a
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        href="https://www.anagrafenazionale.interno.it/"
+                        className="underline">
+                        ANPR
+                    </a>{' '}
+                    e il progetto{' '}
+                    <a
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        href="https://www.agid.gov.it/it/dati/vocabolari-controllati"
+                        className="underline">
+                        OntoPiA
+                    </a>
+                    .
                 </p>
                 <p className="mb-12">
-                    Usa i controlli sottostanti per selezionare la risorsa geografica di interesse.
-                    Seleziona il paese, l'edizione dei dati e il livello di suddivisione amministrativa.
-                    Eventualmente seleziona anche il territorio specifico e la sotto suddivisione amministrativa.
-                    Puoi vedere i confini amministrativi nella mappa qui a fianco.
-                    Ogni territorio è cliccabile e il tooltip mostra i dati associati.
-                    Puoi scaricare la risorsa geografica selezionata in uno qualsiasi dei formati supportati.
-                    Tutte le risorse qui presenti sono rilasciate con <a rel="noopener noreferrer" target="_blank"
-                        href="https://www.agid.gov.it/it/dati/open-data" className="underline">licenza Open Data</a>.
+                    Usa i controlli sottostanti per selezionare la risorsa
+                    geografica di interesse. Seleziona il paese, l'edizione dei
+                    dati e il livello di suddivisione amministrativa.
+                    Eventualmente seleziona anche il territorio specifico e la
+                    sotto suddivisione amministrativa. Puoi vedere i confini
+                    amministrativi nella mappa qui a fianco. Ogni territorio è
+                    cliccabile e il tooltip mostra i dati associati. Puoi
+                    scaricare la risorsa geografica selezionata in uno qualsiasi
+                    dei formati supportati. Tutte le risorse qui presenti sono
+                    rilasciate con{' '}
+                    <a
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        href="https://www.agid.gov.it/it/dati/open-data"
+                        className="underline">
+                        licenza Open Data
+                    </a>
+                    .
                 </p>
                 <Controls />
             </section>
