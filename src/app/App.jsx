90878229c22d6abda6c999abe812637be104e8ec
Alan Sprecacenere <alan@qreate.it>
2024-06-06 15:44:45 +0200

    Refactoring sezione in React (#34)
    * spostamento della parte interattiva in un componente autonomo
    
    * formattazione e linting
    
    * corretta mancanza di key
    
    * rimozione delle funzioni anonime
    
    * spostamento sezione info
    
    ---------
    
    Co-authored-by: Alessio Cimarelli <alessio.cimarelli@sparkfabrik.com>

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,116 +1,237 @@
-import { useEffect, useRef, useState } from "react";
-import Select from "react-select";
+import { useEffect, useRef, useState } from 'react';
+import Select from 'react-select';
 
 export function App() {
-    const selectRefs = useRef({});
-    const downloadRef = useRef();
+    const initialCenter = [42.5206995, 13.2275392];
+    const initialZoom = 6;
 
-    const firstResource = `${window.API_DOMAIN}/api/v2/index.json`;
-    const geoResourceType = "application/geo+json";
-    const labels = ["country", "release", "division", "territory", "subdivision"];
-    const profiles = labels.map(s => `/api/v2/hal-${s}.schema.json`);
+    let map;
+    let mapLayer;
 
-    const [options, setOptions] = useState({});
-    const [resources, setResources] = useState([]);
-    const [downloadPath, setDownloadPath] = useState('#');
-
-    function loadOptions(src) {
-        if (src) {
-            fetch(src)
-                .then(res => res.json())
-                .then(data => {
-                    const items = data._links.item ?? [];
-                    const enclosures = data._links.enclosure ?? [];
-                    const geoResource = enclosures.length && enclosures.find(e => e.type === geoResourceType)?.href;
-
-                    if (items.length > 0) {
-                        const target = items[0].profile;
-                        const targetIndex = profiles.findIndex(p => p === target);
-
-                        setOptions(oldOptions => ({
-                            ...oldOptions,
-                            [target]: items.map(item => ({
-                                value: `${window.API_DOMAIN}${item.href}`,
-                                label: item.title
-                            })),
-                            ...profiles.slice(targetIndex + 1).reduce((options, target) => ({
-                                ...options,
-                                [target]: []
-                            }), {})
-                        }));
-
-                        profiles.slice(targetIndex).forEach(p => selectRefs.current[p]?.clearValue?.());
-                    }
-
-                    window.updateMap(geoResource ? `${window.API_DOMAIN}${geoResource}` : '');
-                    setResources(enclosures.length ? enclosures.map(e => ({ value: e.href, label: e.title })) : []);
-                    downloadRef.current?.clearValue?.();
-                    setDownloadPath('#');
-                });
-        }
+    function ControlsItem({ id, label, children }) {
+        return (
+            <li className="flex flex-col gap-2">
+                <label className="capitalize" htmlFor={id}>
+                    {label}
+                </label>
+                {children}
+            </li>
+        );
     }
 
-    useEffect(() => {
-        loadOptions(firstResource);
-    }, []);
+    function Controls() {
+        const selectRefs = useRef({});
+        const downloadRef = useRef();
 
-    return (
-        <ol className="flex flex-col gap-6 mb-12">
-            {
-                profiles.map((p, i) => (
-                    <li key={p} className="flex flex-col gap-2">
-                        <label className="capitalize" htmlFor={p}>{labels[i]}</label>
-                        <Select
-                            id={p}
-                            ref={el => (selectRefs.current[p] = el)}
-                            name={p}
-                            placeholder={`Select a ${labels[i]}...`}
-                            isDisabled={!options[p]?.length}
-                            options={options[p] ?? []}
-                            onChange={e => loadOptions(e?.value)}
-                        />
-                    </li>
-                ))
+        const firstResource = `${window.API_DOMAIN}/api/v2/index.json`;
+        const geoResourceType = 'application/geo+json';
+        const labels = [
+            'country',
+            'release',
+            'division',
+            'territory',
+            'subdivision',
+        ];
+        const profiles = labels.map((s) => `/api/v2/hal-${s}.schema.json`);
+
+        const [options, setOptions] = useState({});
+        const [resources, setResources] = useState([]);
+        const [downloadPath, setDownloadPath] = useState('#');
+
+        async function loadOptions(src) {
+            if (!src) return;
+
+            const res = await fetch(src);
+            const data = await res.json();
+            const items = data._links.item ?? [];
+            const enclosures = data._links.enclosure ?? [];
+            const geoResource =
+                enclosures.length &&
+                enclosures.find((e) => e.type === geoResourceType)?.href;
+
+            if (items.length) {
+                const target = items[0].profile;
+                const targetIndex = profiles.findIndex((p) => p === target);
+
+                setOptions((oldOptions) => ({
+                    ...oldOptions,
+                    [target]: items.map((item) => ({
+                        value: `${window.API_DOMAIN}${item.href}`,
+                        label: item.title,
+                    })),
+                    ...profiles.slice(targetIndex + 1).reduce(
+                        (options, target) => ({
+                            ...options,
+                            [target]: [],
+                        }),
+                        {}
+                    ),
+                }));
+
+                profiles
+                    .slice(targetIndex)
+                    .forEach((p) => selectRefs.current[p]?.clearValue?.());
             }
 
-            <li key="format" className="flex flex-col gap-2">
-                <label className="capitalize" htmlFor="format">Format</label>
-                <Select
-                    id="format"
-                    ref={downloadRef}
-                    name="format"
-                    placeholder="Select a file format..."
-                    isDisabled={!resources.length}
-                    options={resources}
-                    onChange={e => setDownloadPath(e?.value ?? '#')}
-                />
-            </li>
+            updateMap(geoResource ? `${window.API_DOMAIN}${geoResource}` : '');
+            setResources(
+                enclosures.length
+                    ? enclosures.map((e) => ({ value: e.href, label: e.title }))
+                    : []
+            );
+            downloadRef.current?.clearValue?.();
+            setDownloadPath('#');
+        }
 
-            <li key="download" className="flex flex-col gap-2">
-                {resources.length > 0 && downloadPath !== '#'
-                    ? (
-                        <a
-                            className="bg-ored text-oblue-dark font-bold py-2 px-4 rounded inline-flex items-center justify-between"
-                            target="_blank"
-                            href={`${window.API_DOMAIN}${downloadPath}`}
-                            download={`ondata_confini_amministrativi_${downloadPath.replace(/^\//, '').replace(/\//g, '_')}`}
-                        >
-                            <span>Download</span>
-                            <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
-                            </svg>
-                        </a>
+        async function updateMap(geoResource) {
+            if (geoResource) {
+                // Fetch GeoJSON data
+                const res = await fetch(geoResource);
+                const geojson = await res.json();
+
+                // Remove previous layer
+                mapLayer && map.removeLayer(mapLayer);
+
+                // Add new layer
+                mapLayer = L.geoJson(geojson, {
+                    style: () => ({
+                        color: '#eb593c',
+                        weight: 2,
+                        fillColor: '#2e85d1',
+                        fillOpacity: 0.66,
+                    }),
+                })
+                    .bindPopup(
+                        (layer) => `
+                    <table>
+                        ${Object.entries(layer.feature.properties)
+                            .sort(([a], [b]) => a.localeCompare(b))
+                            .map(
+                                ([k, v]) =>
+                                    `<tr><th>${k}</th><td>${v}</td></tr>`
+                            )
+                            .join('\n')}
+                    </table>
+                `
                     )
-                    : (
-                        <button className="bg-oblue-dark text-oblue-light border-oblue-dark font-bold py-2 px-4 rounded inline-flex items-center justify-between opacity-50 cursor-not-allowed">
-                            <span>Download</span>
-                            <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
-                            </svg>
-                        </button>
-                    )
-                }
-            </li>
-        </ol>
+                    .addTo(map);
+
+                // Adapt map zoom and pan
+                map.fitBounds(mapLayer.getBounds());
+            } else {
+                mapLayer && map.removeLayer(mapLayer);
+                map.setView(initialCenter, initialZoom);
+            }
+        }
+
+        useEffect(() => {
+            loadOptions(firstResource);
+        }, []);
+
+        return (
+            <>
+                <ol className="flex flex-col gap-6">
+                    {profiles.map((p, i) => (
+                        <ControlsItem key={p} id={p} label={labels[i]}>
+                            <Select
+                                id={p}
+                                ref={(el) => (selectRefs.current[p] = el)}
+                                name={p}
+                                placeholder={`Select a ${labels[i]}...`}
+                                isDisabled={!options[p]?.length}
+                                options={options[p] ?? []}
+                                onChange={(e) => loadOptions(e?.value)}
+                            />
+                        </ControlsItem>
+                    ))}
+                </ol>
+
+                {resources.length > 0 && (
+                    <ol className="flex flex-col gap-6">
+                        <ControlsItem key="format" id="format" label="Format">
+                            <Select
+                                id="format"
+                                ref={downloadRef}
+                                name="format"
+                                placeholder="Select a file format..."
+                                isDisabled={!resources.length}
+                                options={resources}
+                                onChange={(e) => setDownloadPath(e?.value ?? '#')}
+                            />
+                        </ControlsItem>
+
+                        
+                        <li key="download" className="flex flex-col gap-2">
+                            {downloadPath !== '#' ? (
+                                <a
+                                    className="bg-ored text-oblue-dark font-bold py-2 px-4 rounded inline-flex items-center justify-between"
+                                    target="_blank"
+                                    href={`${window.API_DOMAIN}${downloadPath}`}
+                                    download={`ondata_confini_amministrativi_${downloadPath
+                                        .replace(/^\//, '')
+                                        .replace(/\//g, '_')}`}>
+                                    <span>Download</span>
+                                    <svg
+                                        className="fill-current w-4 h-4 mr-2"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 20 20">
+                                        <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+                                    </svg>
+                                </a>
+                            ) : (
+                                <button className="bg-oblue-dark text-oblue-light border-oblue-dark font-bold py-2 px-4 rounded inline-flex items-center justify-between opacity-50 cursor-not-allowed">
+                                    <span>Download</span>
+                                    <svg
+                                        className="fill-current w-4 h-4 mr-2"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 20 20">
+                                        <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+                                    </svg>
+                                </button>
+                            )}
+                        </li>
+                    </ol>
+                )}
+            </>
+        );
+    }
+
+    function Map() {
+        const mapRef = useRef(null);
+
+        useEffect(() => {
+            // Setup map
+            map = L.map(mapRef.current, { scrollWheelZoom: false }).setView(
+                initialCenter,
+                initialZoom
+            );
+
+            // Setup tiles
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution:
+                    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+            }).addTo(map);
+
+            // Add watermark
+            L.control.watermark({ position: 'bottomleft' }).addTo(map);
+
+            return () => map.remove();
+        }, []);
+
+        return (
+            <div
+                ref={mapRef}
+                className="grow overflow-hidden max-sm:hidden"></div>
+        );
+    }
+
+    return (
+        <main className="min-h-screen bg-oblue-light text-oblue-dark flex flex-row justify-center">
+            <section className="p-6 max-w-sm flex flex-col justify-between">
+                <Controls />
+            </section>
+            <Map />
+        </main>
     );
 }
--- src/app/index.js
+++ src/app/index.js
@@ -1,99 +1,38 @@
-import { createRoot } from "react-dom/client";
-import { App } from "./App";
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
 
 // Constants
-window.API_DOMAIN = process.env.NODE_ENV !== 'production' ? 'http://localhost:8080' : '';
+window.API_DOMAIN =
+    process.env.NODE_ENV !== 'production' ? 'http://localhost:8080' : '';
 
-// Current year
-(function () {
-    document.getElementById('year').innerText = (new Date()).getFullYear();
-})();
+// Fill current year
+document.getElementById('year').innerText = new Date().getFullYear();
 
-// Swagger UI
-(function () {
-    window.ui = SwaggerUIBundle({
-        url: `${window.API_DOMAIN}/api/v2/openapi.v2.yml`,
-        dom_id: '#docs',
-        deepLinking: true,
-        presets: [
-            SwaggerUIBundle.presets.apis,
-            SwaggerUIStandalonePreset
-        ],
-        plugins: [
-            SwaggerUIBundle.plugins.DownloadUrl
-        ],
-        layout: "BaseLayout"
-    });
-})();
+// Init Swagger UI
+window.ui = SwaggerUIBundle({
+    url: `${window.API_DOMAIN}/api/v2/openapi.v2.yml`,
+    dom_id: '#api',
+    deepLinking: true,
+    presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+    plugins: [SwaggerUIBundle.plugins.DownloadUrl],
+    layout: 'BaseLayout',
+});
 
-// Leaflet
-(function () {
-    // Leaflet map initialization
-    let layer;
-    const initialCentroid = [42.5206995, 13.2275392];
-    const initialZoom = 6;
-    const map = L.map('map', { scrollWheelZoom: false }).setView(initialCentroid, initialZoom);
+// Register leaflet's watermark control
+L.Control.Watermark = L.Control.extend({
+    onAdd: () => {
+        var img = L.DomUtil.create('img');
+        img.src =
+            'https://www.ondata.it/wp-content/uploads/2022/03/logo-onData-corretto-trasparente.png';
+        img.style.width = '125px';
+        return img;
+    },
+});
 
-    // Tile layer loading
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+L.control.watermark = function (opts) {
+    return new L.Control.Watermark(opts);
+};
 
-    // Watermark
-    L.Control.Watermark = L.Control.extend({
-        onAdd: () => {
-            var img = L.DomUtil.create('img');
-            img.src = 'https://www.ondata.it/wp-content/uploads/2022/03/logo-onData-corretto-trasparente.png';
-            img.style.width = '125px';
-            return img;
-        }
-    });
-
-    L.control.watermark = function (opts) {
-        return new L.Control.Watermark(opts);
-    }
-
-    L.control.watermark({ position: 'bottomleft' }).addTo(map);
-
-    window.updateMap = async (geoResource) => {
-        if (geoResource) {
-            // Fetch GeoJSON data
-            const response = await fetch(geoResource);
-            const geojson = await response.json();
-
-            // Layer loading
-            layer && map.removeLayer(layer);
-            layer = L.geoJson(
-                geojson,
-                {
-                    style: function (feature) {
-                        return {
-                            color: "#EB593C",
-                            weight: 2,
-                            fillColor: "#2E85D1",
-                            fillOpacity: 0.66
-                        };
-                    }
-                }
-            ).bindPopup(layer => `
-                <table>
-                    ${Object.entries(layer.feature.properties).sort(([a], [b]) => a.localeCompare(b)).map(([k, v]) => `<tr><th>${k}</th><td>${v}</td></tr>`).join("\n")}
-                </table>
-            `).addTo(map);
-
-            // Adapt map zoom and pan
-            map.fitBounds(layer.getBounds());
-        } else {
-            layer && map.removeLayer(layer);
-            map.setView(initialCentroid, initialZoom);
-        }
-
-        map.invalidateSize();
-    };
-})();
-
-// React
-const container = document.getElementById("controls");
-const root = createRoot(container);
-root.render(<App />);
+// Init app
+const appRoot = createRoot(document.getElementById('/map'));
+appRoot.render(<App />);
